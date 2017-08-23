6fe4399a72cf6bd384b035cd9c73c660beffb271
coyotepod <coyotepod@gmail.com>
2017-08-23 12:50:17 -0400

    Renamed assets

--- Assets/Scripts/CharacterClasses.cs
+++ Assets/Scripts/CharacterClasses.cs
@@ -9,9 +9,13 @@ public class CharacterClasses : MonoBehaviour {
 		
 	}
     string ClericDescription = "A priestly champion who wields divine magic in service of a higher power.";
+    string ClericFeatures = "";
     string FighterDescription = " master of martial combat, skilled with a variety of weapons and armor";
+    string FighterFeatures = "";
     string RogueDescription = "A scoundrel who uses stealth and trickery to overcome obstacles and enemies";
+    string RogueFeatures = "";
     string WizardDescription = "A scholarly magic-user capable of manipulating the structures of reality";
+    string WizardFeatures = "";
     // Update is called once per frame
     void Update () {
 		
