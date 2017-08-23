591cbf1d26cf15fbe2c1a57b225e526eb236bfae
coyotepod <coyotepod@gmail.com>
2017-08-23 10:16:50 -0400

    Updated Assets

---
+++ Assets/Scripts/CharacterClasses.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterClasses : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    string ClericDescription = "A priestly champion who wields divine magic in service of a higher power.";
+    string FighterDescription = " master of martial combat, skilled with a variety of weapons and armor";
+    string RogueDescription = "A scoundrel who uses stealth and trickery to overcome obstacles and enemies";
+    string WizardDescription = "A scholarly magic-user capable of manipulating the structures of reality";
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
